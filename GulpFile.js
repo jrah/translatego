1eb98d4ac92544a186a649e895a3a577086df4ca
jrah <jjames.home@gmail.com>
2017-02-04 15:41:25 +0700

    Add production ready CSS/JS

--- GulpFile.js
+++ GulpFile.js
@@ -1,7 +1,15 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var minify      = require('gulp-minify');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
 
+
+// var
+var cssDirectory = '/css';
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -32,4 +40,33 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('build-js', function() {
+  gulp.src('js/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['-min.js']
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-css', function() {
+  gulp.src('css/*.css')
+      .pipe(sourcemaps.init())
+      .pipe(autoprefixer())
+      .pipe(concat('main.css'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(csso({
+         restructure: false,
+         sourceMap: true,
+         debug: true
+       }))
+      .pipe(gulp.dest('dist'));
+});
+
+
 gulp.task('default', ['serve']);
+gulp.task('build', ['build-js', 'build-css']);
